d671dbf73a05c404e4902ed16e5b6e4b55428321
jelly-boop <jellyjelly111@naver.com>
2021-03-14 02:36:18 +0900

    commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,52 @@
-import logo from './logo.svg';
+import React, {Component} from 'react';
 import './App.css';
+import TOC from "./components/TOC";
+import Content from "./components/Content";
+import Subject from "./components/Subject";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+//리액트의 컴포넌트 클래스를 상속하여 새로운 클래스를 만드는 것!
+//즉 컴포넌트를 만드는 코드임!
+class App extends Component {
+  constructor(props){
+    super(props);
+    this.state={
+      mode:'welcome',
+      subject:{title:'WEB', sub:'World Wide Web'},
+      welcome:{title:'welcome',desc:'Hello React!!'},
+      toc:{title:'HTML',desc:'HyperTextMarkup Language'},
+      contents:[//여러개이기 때문에 배열을 만든다.
+        {id:1, title:'HTML', desc:'HTML is for information'},
+        {id:2, title:'CSS', desc:'CSS is for design'},
+        {id:3, title:'JavaScript', desc:'JavaScript is for interactive'}
+      ]
+    }
+  }
+  render() {
+    var _title=null;
+    var _desc=null;
+
+    if(this.state.mode==='welcome'){
+      _title=this.state.welcome.title;
+      _desc=this.state.welcome.desc;
+    }else if(this.state.mode==='read'){
+      _title=this.state.contents[0].title;
+      _desc=this.state.contents[0].desc;
+    }
+    return (
+      <div className="App">
+        <Subject 
+        title={this.state.subject.title} 
+        sub={this.state.subject.sub}
+        onChangePage={function(){
+         this.setState({mode:'welcome'});
+        }.bind(this)}>
+        </Subject>
+        <TOC data={this.state.contents}></TOC>
+        <Content title={_title} desc={_desc}></Content>
+      </div>
+    );
+  }
 }
 
 export default App;
---
+++ src/components/TOC.js
@@ -0,0 +1,24 @@
+import React, {Component} from 'react';
+
+class TOC extends Component {
+
+  render(){
+      var lists=[];
+      var data=this.props.data;
+      var i=0;
+      while(i<data.length){
+        lists.push(<li key={data[i].id}><a href={"/content/"+data[i].id}>{data[i].title}</a></li>)//엘리먼트 자동생성
+        i=i+1;
+      }
+    return (
+     <nav>
+            <ul>
+               {lists}
+            
+            </ul>
+        </nav> 
+    );
+  }
+}
+
+export default TOC;
